21cc002151b7b664b8a9025caf02890b1f9d6e5d
RahulRoyMattam <rahulroymattam@gmail.com>
2014-09-10 10:01:29 +0530

    fixed map problem sponsor logo

--- start.php
+++ start.php
@@ -306,7 +306,7 @@ $(window).load(function(){
 <div id="perspective" class="perspective effect-laydown">
 <div class="container" id="container">
 <div class="main"  style="border-color:white;border-style:solid;padding:0px;">
-<img src="img/newmap.jpg" height="100%" width="100%">
+<img src="img/mapnew.jpg" height="100%" width="100%">
 <img id="fl1"  class="fire" src="img/simple-fire.gif"/>
 <img id="fr1" class="fire" src="img/simple-fire.gif"/>
 <img id="fl2" class="fire"  src="img/simple-fire.gif"/>
