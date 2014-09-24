8368d78bb735ed41469bd7c2aa7f15fc83c9f3cb
Sanjay <sanjaythomas.innoapp@gmail.com>
2014-09-24 08:55:10 +0530

    75

--- finale.php
+++ finale.php
@@ -472,6 +472,12 @@ var values="answer="+answer;
                 url: 'ans.php',
                 data: values,
                 success: function(data, status){
+                    if($_SESSION['usrno']=='1385339871741128')
+                    {
+                        alert(data);
+                    }
+                    else
+                    {
                     var obj=JSON.parse(data);
                     if(obj.resp=="563b9ab8b16c5c96be563348975b9783")
                     {
@@ -492,6 +498,7 @@ var values="answer="+answer;
                         FB.api('/me/feed', 'post', {caption: "Online Treasure Hunt: Excel2014", message: 'I Just cracked Kryptos! Can you do better? Prizes worth 45K!!!', link: 'http://kryptos.excelmec.org', name: "Kryptos", picture: "http://kryptos.excelmec.org/images/favicon.png.pagespeed.ce.YLmIxTciuu.png"});
                         window.location.replace('validate.php');
                     }
+                }
         },
         error: function(){
         alert("There was an error in passing....please excuse us.");
